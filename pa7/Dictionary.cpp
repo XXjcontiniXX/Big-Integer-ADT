049c1b041a8daecc0502bbad38d831abbdeb6ed8
James Contini <jcontini@ucsc.edu>
2023-05-31 17:58:56 -0700

    technically finished all functions

--- pa7/Dictionary.cpp
+++ pa7/Dictionary.cpp
@@ -3,7 +3,7 @@
 #include"Dictionary.h"
 
 using namespace std;
-	
+		
 	Dictionary::Node::Node(keyType k, valType v) {
 		this->key = k;
 		this->val = v;
@@ -53,7 +53,7 @@ using namespace std;
    // postOrderDelete()
    // Deletes all Nodes in the subtree rooted at R.
    void Dictionary::postOrderDelete(Node* R) {
-		if ( R != nullptr) {
+		if ( R != nullptr && R != nil) {
          postOrderDelete(R->left);
          postOrderDelete(R->right);
 			delete R;
@@ -74,14 +74,14 @@ using namespace std;
 				return x;
 			}
 		}
-		return nullptr;
+		return nil;
 	}
 
    // findMin()
    // If the subtree rooted at R is not empty, returns a pointer to the 
    // leftmost Node in that subtree, otherwise returns nil.
    Dictionary::Node* Dictionary::findMin(Node* R) {
-		Node * x = nullptr;
+		Node * x = nil;
 		while (x->left != nullptr) {
       	x = x->left;
 		}
@@ -92,7 +92,7 @@ using namespace std;
    // If the subtree rooted at R is not empty, returns a pointer to the 
    // rightmost Node in that subtree, otherwise returns nil.
    Dictionary::Node* Dictionary::findMax(Node* R) {
-		Node * x = nullptr;
+		Node * x = nil;
       while (x->right != nullptr) {
          x = x->right;
       }
@@ -151,7 +151,9 @@ using namespace std;
    
    // Creates new Dictionary in the empty state. 
    Dictionary::Dictionary() {
-		nil = nullptr;
+		nil = new Node("0", 0);
+		nil->left = root;
+		nil->right = root;
 		root = nullptr;
 		current = nullptr;
 		num_pairs = 0;
@@ -159,59 +161,90 @@ using namespace std;
 
    // Copy constructor.
    Dictionary::Dictionary(const Dictionary& D) {
+		nil = new Node("0", 0);
+		nil->left = root;
+      nil->right = root;
+		root = nullptr;
+		current = nullptr;
 		this->preOrderCopy(D.root, nullptr);
+		num_pairs = D.num_pairs;
 	}
 
    // Destructor
    Dictionary::~Dictionary() {
 		postOrderDelete(this->root);
 	}
-/*
+
 
    // Access functions --------------------------------------------------------
 
    // size()
    // Returns the size of this Dictionary.
-   int size() const;
+   int Dictionary::size() const {
+		return num_pairs;
+	}
 
    // contains()
    // Returns true if there exists a pair such that key==k, and returns false
    // otherwise.
-   bool contains(keyType k) const;
+   bool Dictionary::contains(keyType k) const {
+		return search(this->root, k) == nil ? false : true;
+	}
 
    // getValue()
    // Returns a reference to the value corresponding to key k.
    // Pre: contains(k)
-   valType& getValue(keyType k) const;
+   valType& Dictionary::getValue(keyType k) const {
+		if ( !this->contains(k) ) {
+			cout << "throw an error\n";
+		}
+		return (search(this->root, k))->val;	
+	}
 
    // hasCurrent()
    // Returns true if the current iterator is defined, and returns false 
    // otherwise.
-   bool hasCurrent() const;
+   bool Dictionary::hasCurrent() const {
+		return this->current == nullptr || this->current == nil ? false : true;
+	}
 
    // currentKey()
    // Returns the current key.
    // Pre: hasCurrent() 
-   keyType currentKey() const;
+   keyType Dictionary::currentKey() const {
+		if ( !hasCurrent() ) {
+			cout << "no current throw error\n";
+		}
+		return current->key;
+	}
 
    // currentVal()
    // Returns a reference to the current value.
    // Pre: hasCurrent()
-   valType& currentVal() const;
+   valType& Dictionary::currentVal() const {
+		if ( !hasCurrent() ) {
+         cout << "no current throw an error\n";
+      }
+      return current->val;
+	}
 
 
    // Manipulation procedures -------------------------------------------------
 
    // clear()
    // Resets this Dictionary to the empty state, containing no pairs.
-   void clear();
-*/
+   void Dictionary::clear() {
+		postOrderDelete(this->root);
+		current = nullptr;
+		root = nullptr;
+	}
+
    // setValue()
    // If a pair with key==k exists, overwrites the corresponding value with v, 
    // otherwise inserts the new pair (k, v).
    void Dictionary::setValue(keyType k, valType v) {
 		Node* N = search(this->root, k);
-		if (N != nullptr) { 
+		if (N != nil) { 
 			N->val = v;
 		}else{ 				
 			Node* y = nullptr;
@@ -228,43 +261,75 @@ using namespace std;
          z->parent = y;
          if (y == nullptr) {
             this->root = z;    // tree T was empty
+				this->root->parent = nil;
+				this->num_pairs += 1;
          } else if ( k.compare(y->key) < 0 ) {
+				if (y->left == nullptr) { this->num_pairs += 1;}
             y->left = z;
          } else {
+				if (y->right == nullptr) { this->num_pairs += 1;}
             y->right = z;
 			}
 		}
 	}
-/*
+
    // remove()
    // Deletes the pair for which key==k. If that pair is current, then current
    // becomes undefined.
    // Pre: contains(k).
-   void remove(keyType k);
+   void Dictionary::remove(keyType k) {
+		if ( !contains(k) ) {
+			cout << "throw a mf error\n";
+		}
+	
+		Node* cur = search(this->root, k);
+
+		if ( cur == current ) {
+			delete current;
+			current = nullptr;
+			return;
+		}
+		delete cur;
+		return;
+	}
 
    // begin()
    // If non-empty, places current iterator at the first (key, value) pair
    // (as defined by the order operator < on keys), otherwise does nothing. 
-   void begin();
+   void Dictionary::begin() {
+		current = nil->right;
+	}
 
    // end()
    // If non-empty, places current iterator at the last (key, value) pair
    // (as defined by the order operator < on keys), otherwise does nothing. 
-   void end();
+   void Dictionary::end() {
+		current = findMax(this->root);
+	}
 
    // next()
    // If the current iterator is not at the last pair, advances current 
    // to the next pair (as defined by the order operator < on keys). If 
    // the current iterator is at the last pair, makes current undefined.
    // Pre: hasCurrent()
-   void next();
+   void Dictionary::next() {
+		if ( !hasCurrent() ) {
+			cout << "throw error\n";
+		}
+		current = findNext(current);
+	}
 
    // prev()
    // If the current iterator is not at the first pair, moves current to  
    // the previous pair (as defined by the order operator < on keys). If 
    // the current iterator is at the first pair, makes current undefined.
    // Pre: hasCurrent()
-   void prev();
+   void Dictionary::prev() {
+	if ( !hasCurrent() ) {
+         cout << "throw error\n";
+      }
+      current = findPrev(current);
+	}
 
 
    // Other Functions ---------------------------------------------------------
@@ -274,20 +339,41 @@ using namespace std;
    // pairs are separated by a newline "\n" character, and the items key and value 
    // are separated by the sequence space-colon-space " : ". The pairs are arranged 
    // in order, as defined by the order operator <.
-   std::string to_string() const;
+   std::string Dictionary::to_string() const {
+		string s;
+		this->inOrderString(s, this->root);
+		return s;
+	}
 
    // pre_string()
    // Returns a string consisting of all keys in this Dictionary. Consecutive
    // keys are separated by newline "\n" characters. The key order is given
    // by a pre-order tree walk.
-   std::string pre_string() const;
+   std::string Dictionary::pre_string() const {
+		string s;
+      this->preOrderString(s, this->root);
+      return s;
+	}
 
    // equals()
    // Returns true if and only if this Dictionary contains the same (key, value)
    // pairs as Dictionary D.
-   bool equals(const Dictionary& D) const;
+   bool Dictionary::equals(const Dictionary& D) const {
+		if (D.size() != this->size()) {
+			return false;
+		}
+		string s;
+		this->inOrderString(s, this->root);
+		for (unsigned long i = 0; i < s.size(); i += 1) {
+			string l;
+			l += s[i];
+			if ( D.contains( l ) == false) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-*/
    // Overloaded Operators ----------------------------------------------------
    
    // operator<<()
@@ -300,16 +386,27 @@ using namespace std;
 		return stream << s;
 		;
 	}
-/*
+
    // operator==()
    // Returns true if and only if Dictionary A equals Dictionary B, as defined
    // by member function equals(). 
-   friend bool operator==( const Dictionary& A, const Dictionary& B );
+   bool operator==( const Dictionary& A, const Dictionary& B ) {
+		return A.equals(B);
+	}
 
    // operator=()
    // Overwrites the state of this Dictionary with state of D, and returns a
    // reference to this Dictionary.
-   Dictionary& operator=( const Dictionary& D );
-*/
+   Dictionary& Dictionary::operator=( const Dictionary& D ) {
+		this->clear();
+		this->preOrderCopy(D.root, nullptr);
+		this->num_pairs = D.num_pairs;
+		this->nil = new Node("0", 0);
+		nil->right = root;
+		nil->left = root;
+		nil->parent = nullptr;
+		return *this;
+	}
+
 
 
--- pa7/DictionaryTest.cpp
+++ pa7/DictionaryTest.cpp
@@ -7,11 +7,17 @@ using namespace std;
 
 int main () {
 	Dictionary A = Dictionary();
+	Dictionary B = Dictionary();
 	A.setValue("b", 0);
 	A.setValue("a", 0);
 	A.setValue("c", 0);
 	A.setValue("d", 0);
 	A.setValue("e", 0);
-	cout << A;
+	cout << A;	
+	B = A;
+	//cout << "\n";
+	//cout << B;
+	B = Dictionary(B);
+	cout << B;
 	return 0;
 }
